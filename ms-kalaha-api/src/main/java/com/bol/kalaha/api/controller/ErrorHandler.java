3779077ef20699b46b3b85a7aa59ce8bcea898a0
Heydar Rzayev <rzaaeeff@gmail.com>
2022-08-27 23:34:28 +0400

    feat: Add exception handling for game core
    Change-Id: I12e6d7fada8d158565fb43bdbc1385ae8bcf0b11

--- ms-kalaha-api/src/main/java/com/bol/kalaha/api/controller/ErrorHandler.java
+++ ms-kalaha-api/src/main/java/com/bol/kalaha/api/controller/ErrorHandler.java
@@ -2,12 +2,14 @@ package com.bol.kalaha.api.controller;
 
 import com.bol.kalaha.api.logger.RzLogger;
 import com.bol.kalaha.api.model.dto.ExceptionResponseDto;
+import com.bol.kalaha.core.exception.IllegalMoveException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
+import static com.bol.kalaha.api.model.constant.Constants.ILLEGAL_MOVE_EXCEPTION_CODE;
 import static com.bol.kalaha.api.model.constant.Constants.UNEXPECTED_EXCEPTION_CODE;
 
 @RestControllerAdvice
@@ -18,8 +20,15 @@ public class ErrorHandler extends ResponseEntityExceptionHandler {
 
     @ExceptionHandler(Exception.class)
     public ResponseEntity<ExceptionResponseDto> handle(Exception ex) {
-        var response = new ExceptionResponseDto(UNEXPECTED_EXCEPTION_CODE + "unexpected", null);
+        var response = new ExceptionResponseDto(UNEXPECTED_EXCEPTION_CODE, null);
         logger.error(ERROR_MESSAGE_TEMPLATE, ex.getClass().getSimpleName(), ex.getMessage(), ex);
         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(response);
+    }
+
+    @ExceptionHandler(IllegalMoveException.class)
+    public ResponseEntity<ExceptionResponseDto> handleIllegalMove(IllegalMoveException ex) {
+        var response = new ExceptionResponseDto(ILLEGAL_MOVE_EXCEPTION_CODE, ex.getMessage());
+        logger.error(ERROR_MESSAGE_TEMPLATE, ex.getClass().getSimpleName(), ex.getMessage(), ex);
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(response);
     }
 }
--- ms-kalaha-api/src/main/java/com/bol/kalaha/api/model/constant/Constants.java
+++ ms-kalaha-api/src/main/java/com/bol/kalaha/api/model/constant/Constants.java
@@ -1,6 +1,7 @@
 package com.bol.kalaha.api.model.constant;
 
 public class Constants {
-    private static final String EXCEPTION_CODE_PREFIX = "quiz.exception.";
+    private static final String EXCEPTION_CODE_PREFIX = "kalaha.exception.";
     public static final String UNEXPECTED_EXCEPTION_CODE = EXCEPTION_CODE_PREFIX + "unexpected";
+    public static final String ILLEGAL_MOVE_EXCEPTION_CODE = EXCEPTION_CODE_PREFIX + "illegal-move";
 }
