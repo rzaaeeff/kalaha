2f01eeb8cfb409a8a2ae39b80d28c8f930a3a341
Heydar Rzayev <rzaaeeff@gmail.com>
2022-08-30 21:42:08 +0400

    docs: Add documentation to DTOs
    Change-Id: I9a8e0f62d99df84281d17c92ff348453644f07e2

--- ms-kalaha-api/src/main/java/com/bol/kalaha/api/model/dto/GameDto.java
+++ ms-kalaha-api/src/main/java/com/bol/kalaha/api/model/dto/GameDto.java
@@ -2,6 +2,8 @@ package com.bol.kalaha.api.model.dto;
 
 import com.bol.kalaha.api.model.enums.GameStatus;
 import com.bol.kalaha.api.model.enums.PlayerID;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -14,16 +16,23 @@ import java.util.List;
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
+@ApiModel("Game model")
 public class GameDto {
+    @ApiModelProperty("ID of the game")
     private String id;
 
+    @ApiModelProperty("Seed count in each house")
     private List<Integer> houses;
 
+    @ApiModelProperty("Seed count in each store")
     private List<Integer> stores;
 
+    @ApiModelProperty("ID of the active player")
     private PlayerID activePlayerId;
 
+    @ApiModelProperty("Current status of the game")
     private GameStatus status;
 
+    @ApiModelProperty("Creation date of the game")
     private LocalDateTime createdAt;
 }
--- ms-kalaha-api/src/main/java/com/bol/kalaha/api/model/dto/GameMoveDto.java
+++ ms-kalaha-api/src/main/java/com/bol/kalaha/api/model/dto/GameMoveDto.java
@@ -1,6 +1,8 @@
 package com.bol.kalaha.api.model.dto;
 
 import com.bol.kalaha.api.model.enums.PlayerID;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -10,7 +12,11 @@ import lombok.NoArgsConstructor;
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
+@ApiModel("Game move model")
 public class GameMoveDto {
+    @ApiModelProperty("ID of the pit to make the move")
     private Integer pitId;
+
+    @ApiModelProperty("ID of the player who makes the move")
     private PlayerID playerId;
 }
